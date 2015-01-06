f8d76ac96fa8d6f7bd3ab9d49bcfe74c98547dbb
Joris Vankerschaver <jvankerschaver@enthought.com>
2015-01-06 16:53:30 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='simplesat',
+    version='0.1',
+    author='Enthought, Inc',
+    author_email='info@enthought.com',
+    url='https://github.com/enthought/sat-solvers',
+    description='Simple SAT solvers for use in Enstaller',
+    packages=['simplesat'],
+)
