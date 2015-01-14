9e4735adb9c334d077406bc84a80498b851b604b
Joris Vankerschaver <jvankerschaver@enthought.com>
2015-01-14 10:24:53 +0000

    Simple Python-based dependency analyzer.

---
+++ simplesat/pysolver_helpers.py
@@ -0,0 +1,36 @@
+from simplesat.api import Solver
+
+
+def solver_from_rules_set(rules):
+    """
+    Construct a SAT solver from a rules generator.
+
+    Parameters
+    ----------
+    pool: Pool
+    rules: RulesGenerator
+
+    Returns
+    -------
+    solver: Solver.
+
+    """
+    s = Solver()
+    for rule in rules:
+        s.add_clause(rule.literals)
+    s._setup_assignments()
+    return s
+
+
+def is_satisfiable(rules):
+    s = solver_from_rules_set(rules)
+    return s.search() is not False
+
+
+def solve_sat(s):
+    solution = s.search()
+    # Return solution as list of signed integers.
+    return sorted(
+        [(+1 if value else -1) * _id for _id, value in solution.items()],
+        key=lambda lit: abs(lit)
+    )
