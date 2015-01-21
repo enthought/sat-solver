65362c17988798b8647a45ce3c65d3a0566c8e67
Joris Vankerschaver <jvankerschaver@enthought.com>
2015-01-21 10:57:49 +0000

    Factor out scenario test tools.

--- simplesat/tests/test_scenarios.py
+++ simplesat/tests/test_scenarios.py
@@ -1,60 +1,21 @@
-import os
 from unittest import TestCase
 
-import yaml
-
-
-from enstaller import Repository
-from enstaller.new_solver.package_parser import PrettyPackageStringParser
-from enstaller.new_solver.pool import Pool
-from enstaller.new_solver.requirement import Requirement
-
-from enstaller.solver import Request
-from enstaller.versions.enpkg import EnpkgVersion
-
 from simplesat.pysolver import optimize
-from simplesat.rules_generator import RulesGenerator
-
-HERE = os.path.dirname(__file__)
+from .test_tools import ScenarioMixin
 
 
-def scenario_factory(filename):
-    with open(filename) as fp:
-        data = yaml.load(fp)
-    packages = data.get('packages', [])
-    request = data['request']
-    assert len(request) == 1
-    step = request[0]
-    assert step['operation'] == 'install'
-    requirement_str = step['requirement']
-    requirement = Requirement._from_string(requirement_str)
-
-    repository = Repository()
-
-    parser = PrettyPackageStringParser(EnpkgVersion.from_string)
-    for package_str in packages:
-        package = parser.parse_to_package(package_str, "2.7")
-        repository.add_package(package)
-
-    return Pool([repository]), requirement
-
-
-class TestSolverSimple(TestCase):
+class TestSolverSimple(TestCase, ScenarioMixin):
 
     def test_simple_numpy(self):
+        # Given
+        scenario = 'simple_numpy.yaml'
+        pool, requirement = self.parse_scenario_file(scenario)
+        rules = self.generate_rules_for_requirement(pool, requirement)
 
-        pool, requirement = scenario_factory(os.path.join(HERE, 'simple_numpy.yaml'))
-
-        request = Request()
-        request.install(requirement)
-
-        rules_generator = RulesGenerator(pool, request)
-        rules = list(rules_generator.iter_rules())
-
+        # When
         solution = optimize(pool, requirement, rules)
 
-        names_and_versions = [
-            (pkg.name, str(pkg.version)) for pkg in solution
-        ]
-        self.assertItemsEqual(names_and_versions,
-                              [('mkl', '10.3-1'), ('numpy', '1.8.1-1')])
+        # Then
+        names_and_versions = [(pkg.name, str(pkg.version)) for pkg in solution]
+        self.assertItemsEqual(
+            names_and_versions, [('mkl', '10.3-1'), ('numpy', '1.8.1-1')])
---
+++ simplesat/tests/test_tools.py
@@ -0,0 +1,76 @@
+import os
+
+import yaml
+
+from enstaller import Repository
+from enstaller.new_solver.package_parser import PrettyPackageStringParser
+from enstaller.new_solver.pool import Pool
+from enstaller.new_solver.requirement import Requirement
+from enstaller.solver import Request
+from enstaller.versions.enpkg import EnpkgVersion
+
+from simplesat.rules_generator import RulesGenerator
+
+
+class ScenarioMixin(object):
+
+    HERE = os.path.dirname(__file__)
+
+    def parse_scenario_file(self, filename):
+        """Parses a YAML scenario file.
+
+        Parameters
+        ----------
+        filename: str
+            Path of a scenario file, relative to the directory of this file.
+
+        Returns
+        -------
+        pool: Pool instance
+            Pool containing the packages contained in the scenario file.
+        requirement: Requirement instance
+            Requirement describing the package to be installed.
+
+        """
+        path = os.path.join(self.HERE, filename)
+        with open(path) as fp:
+            data = yaml.load(fp)
+        packages = data.get('packages', [])
+        request = data['request']
+        assert len(request) == 1
+        step = request[0]
+        assert step['operation'] == 'install'
+        requirement_str = step['requirement']
+        requirement = Requirement._from_string(requirement_str)
+
+        repository = Repository()
+
+        parser = PrettyPackageStringParser(EnpkgVersion.from_string)
+        for package_str in packages:
+            package = parser.parse_to_package(package_str, "2.7")
+            repository.add_package(package)
+
+        return Pool([repository]), requirement
+
+    def generate_rules_for_requirement(self, pool, requirement):
+        """Generate CNF rules for a requirement.
+
+        Parameters
+        ----------
+        pool: Pool
+            Package constraints.
+        requirement: Requirement
+            Package to be installed.
+
+        Returns
+        -------
+        rules: list
+            Package rules describing the given scenario.
+
+        """
+        request = Request()
+        request.install(requirement)
+
+        rules_generator = RulesGenerator(pool, request)
+        rules = list(rules_generator.iter_rules())
+        return rules
