4483b54b249f8d414d6ab5216f2fc62668394add
John Tyree <jtyree@enthought.com>
2015-11-03 12:35:23 -0600

    TST: add tests for AssignmentSet

---
+++ simplesat/sat/tests/test_assignment_set.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from ..assignment_set import AssignmentSet, MISSING
+
+
+class TestAssignmentSet(unittest.TestCase):
+
+    def test_starts_empty(self):
+        AS = AssignmentSet()
+        self.assertEqual(AS.num_assigned, 0)
+        self.assertEqual(len(AS), 0)
+        self.assertEqual({}, AS.get_changelog())
+        self.assertEqual([], AS.keys())
+        self.assertEqual([], AS.values())
+        self.assertEqual([], AS.items())
+
+    def test_num_assigned(self):
+        AS = AssignmentSet()
+
+        AS[0] = None
+        self.assertEqual(AS.num_assigned, 0)
+
+        AS[1] = True
+        self.assertEqual(AS.num_assigned, 1)
+
+        AS[0] = False
+        self.assertEqual(AS.num_assigned, 2)
+
+        AS[1] = None
+        self.assertEqual(AS.num_assigned, 1)
+
+        AS[1] = True
+        self.assertEqual(AS.num_assigned, 2)
+
+        del AS[0]
+        self.assertEqual(AS.num_assigned, 1)
+
+        AS[1] = False
+        self.assertEqual(AS.num_assigned, 1)
+
+        AS[1] = None
+        self.assertEqual(AS.num_assigned, 0)
+
+        del AS[1]
+        self.assertEqual(AS.num_assigned, 0)
+
+    def test_container(self):
+        AS = AssignmentSet()
+
+        AS[0] = True
+        AS[1] = False
+
+        self.assertIn(0, AS)
+        self.assertNotIn(2, AS)
+
+        AS[3] = None
+        AS[2] = True
+        AS[4] = None
+
+        self.assertIn(2, AS)
+        self.assertIn(4, AS)
+
+        del AS[4]
+        self.assertNotIn(4, AS)
+
+        expected = dict(zip(range(4), [True, False, True, None]))
+
+        manual_result = dict(zip(AS.keys(), AS.values()))
+        self.assertEqual(AS.items(), list(expected.items()))
+        self.assertEqual(manual_result, expected)
+        self.assertEqual(len(AS), len(expected))
+
+    def test_copy(self):
+
+        AS = AssignmentSet()
+
+        AS[0] = None
+        AS[1] = True
+        AS[2] = None
+        AS[3] = False
+        AS[4] = True
+
+        expected = {
+            0: None,
+            1: True,
+            2: None,
+            3: False,
+            4: True,
+        }
+
+        copied = AS.copy()
+
+        self.assertIsNot(copied._data, AS._data)
+        self.assertEqual(copied._data, expected)
+
+        expected = {k: (MISSING, v) for k, v in expected.items()}
+
+        self.assertIsNot(copied._changelog, AS._changelog)
+        self.assertEqual(copied._changelog, expected)
+
+        self.assertEqual(copied.num_assigned, AS.num_assigned)
+
+        del AS[1]
+        self.assertIn(1, copied)
+
+    def test_changelog(self):
+
+        AS = AssignmentSet()
+
+        AS[0] = None
+
+        expected = {0: (MISSING, None)}
+        self.assertEqual(AS._changelog, expected)
+
+        AS[1] = True
+        expected[1] = (MISSING, True)
+        self.assertEqual(AS._changelog, expected)
+
+        AS[1] = False
+        expected[1] = (MISSING, False)
+        self.assertEqual(AS._changelog, expected)
+
+        del AS[1]
+        del expected[1]
+        self.assertEqual(AS._changelog, expected)
+
+        # Keep should preserve the log
+        self.assertEqual(AS.get_changelog(), expected)
+        self.assertEqual(AS.get_changelog(), expected)
+
+        # Otherwise clear the log
+        log = AS.consume_changelog()
+        self.assertEqual(log, expected)
+        self.assertEqual(AS.get_changelog(), {})
+
+        # Test when something is already present
+        AS[0] = False
+        expected = {0: (None, False)}
+        self.assertEqual(AS.get_changelog(), expected)
+
+        del AS[0]
+        expected = {0: (None, MISSING)}
+        self.assertEqual(AS.get_changelog(), expected)
