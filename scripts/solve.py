c83cd7a4b356b51cc8a4a14152ad5f94edc0048d
John Tyree <jtyree@enthought.com>
2015-12-09 16:16:25 -0500

    ENH: add a debug flag to the solve script

--- scripts/solve.py
+++ scripts/solve.py
@@ -10,7 +10,7 @@ from simplesat.dependency_solver import DependencySolver
 
 
 def solve_and_print(request, remote_repositories, installed_repository,
-                    print_ids, prune=True):
+                    print_ids, prune=True, debug=False):
     pool = Pool(remote_repositories)
     pool.add_repository(installed_repository)
 
@@ -19,6 +19,8 @@ def solve_and_print(request, remote_repositories, installed_repository,
     transaction = solver.solve(request)
     print(transaction)
     print(solver._last_solve_time, file=sys.stderr)
+    if debug:
+        print(solver._policy._log_report(), file=sys.stderr)
 
 
 def main(argv=None):
@@ -27,13 +29,15 @@ def main(argv=None):
     p = argparse.ArgumentParser()
     p.add_argument("scenario", help="Path to the YAML scenario file.")
     p.add_argument("--print-ids", action="store_true")
-    p.add_argument("--no-prune", action="store_false")
+    p.add_argument("--no-prune", action="store_true")
+    p.add_argument("--debug", action="store_true")
 
     ns = p.parse_args(argv)
 
     scenario = Scenario.from_yaml(ns.scenario)
     solve_and_print(scenario.request, scenario.remote_repositories,
-                    scenario.installed_repository, ns.print_ids, ns.no_prune)
+                    scenario.installed_repository,
+                    ns.print_ids, prune=not ns.no_prune, debug=ns.debug)
 
 
 if __name__ == '__main__':
