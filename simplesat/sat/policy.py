ac5ec9cb9377e82b78d9630fbff9ff270062eaf2
John Tyree <jtyree@enthought.com>
2015-12-09 23:54:56 -0500

    ENH: add policy based on priority queues

--- simplesat/sat/policy.py
+++ simplesat/sat/policy.py
@@ -1,9 +1,15 @@
+from __future__ import print_function
+
 import abc
-from collections import Counter
+from collections import Counter, defaultdict
+import sys
 
 import six
 
 from enstaller.collections import DefaultOrderedDict
+from enstaller.new_solver.requirement import Requirement
+from simplesat.utils.graph import toposort
+from .priority_queue import PriorityQueue, GroupPrioritizer
 
 
 def _pkg_id_to_version(pool, package_id):
@@ -238,6 +244,191 @@ class UndeterminedClausePolicy(IPolicy):
             return min(unassigned_ids)
         else:
             return None
+
+
+class PriorityQueuePolicy(IPolicy):
+
+    """ An IPolicy that uses a priority queue to determine which package id
+    should be suggested next.
+
+    Packages are split into groups:
+
+        1. currently installed,
+        2. newer versions of currently installed,
+        3. explicitly specified as a requirement,
+        4. everything else,
+
+    where each group is arranged in topological order by dependency
+    relationships and then descending order by version number.
+    The groups are then searched in order and the first unassigned package id
+    is suggested.
+    """
+
+    def transitive_deps(self, pkg, dependencies, trans=None):
+        trans = trans if trans is not None else defaultdict(set)
+        if pkg in trans:
+            return trans
+        deps = dependencies[pkg]
+        trans[pkg].update(deps)
+        for dep in deps:
+            self.transitive_deps(dep, dependencies, trans)
+            trans[pkg].update(trans[dep])
+        return trans
+
+    def _rank_packages(self, package_ids):
+        pool = self._pool
+
+        dependencies = defaultdict(set)
+        to_req = Requirement.from_legacy_requirement_string
+        for package_id in package_ids:
+            dependencies[package_id].update(
+                pool.package_id(package)
+                for dep_str in pool._id_to_package[package_id].dependencies
+                for package in pool.what_provides(to_req(dep_str))
+            )
+
+        transitive = defaultdict(set)
+        for pkg_id in package_ids:
+            self.transitive_deps(pkg_id, dependencies, transitive)
+
+        packages_by_name = self._group_packages_by_name(package_ids)
+
+        for package_id in package_ids:
+            package = pool._id_to_package[package_id]
+            deps = dependencies[package_id]
+            group = packages_by_name[package.name]
+            for dep in list(deps):
+                depkg = pool._id_to_package[dep]
+                bad = [pool._id_to_package[p]
+                       for p in transitive[dep].intersection(group)]
+                if bad:
+                    msg = "BAD: {}-{} -> {}-{} -> {}".format(
+                        package.name, package.version,
+                        depkg.name, depkg.version,
+                        ["{}-{}".format(pkg.name, pkg.version) for pkg in bad]
+                    )
+                    print(msg, file=sys.stderr)
+                    deps.remove(dep)
+
+        for package_id in package_ids:
+            package = pool._id_to_package[package_id]
+            others = pool.what_provides(to_req(package.name))
+            other_older = (pool.package_id(other) for other in others
+                           if other.version < package.version)
+            dependencies[package_id].update(other_older)
+
+        ordered = []
+        by_version = six.functools.partial(_pkg_id_to_version, pool)
+        for group in reversed(tuple(toposort(dependencies))):
+            ordered.extend(sorted(group, key=by_version, reverse=True))
+
+        package_id_to_rank = {
+            package_id: rank
+            for rank, package_id in enumerate(ordered)
+        }
+
+        return package_id_to_rank
+
+    def _group_packages_by_name(self, package_ids):
+        pool = self._pool
+
+        name_map = DefaultOrderedDict(list)
+        for package_id in package_ids:
+            package = pool._id_to_package[package_id]
+            name_map[package.name].append(package_id)
+
+        name_to_package_ids = {}
+        by_version = six.functools.partial(_pkg_id_to_version, pool)
+        for name, package_ids in name_map.items():
+            ordered = sorted(package_ids, key=by_version, reverse=True)
+            name_to_package_ids[name] = ordered
+
+        return name_to_package_ids
+
+    def __init__(self, pool, installed_repository, prefer_installed=True):
+        self._pool = pool
+        self._installed_ids = set(map(pool.package_id, installed_repository))
+        self._preferred_ids = set()
+
+        package_ids = pool._id_to_package.keys()
+        package_id_to_rank = self._rank_packages(package_ids)
+        self._name_to_package_ids = self._group_packages_by_name(package_ids)
+
+        for package_id in self._installed_ids:
+            name = pool._id_to_package[package_id].name
+            self._preferred_ids.update(self._name_to_package_ids[name])
+            self._preferred_ids.remove(package_id)
+
+        def key_func(p):
+            return package_id_to_rank[p]
+
+        self._unassigned_pkg_ids = PriorityQueue()
+
+        self.DEFAULT = 0
+        if prefer_installed:
+            self.INSTALLED = -3
+            self.PREFERRED = -2
+            self.REQUIRED = -1
+        else:
+            self.PREFERRED = -3
+            self.REQUIRED = -2
+            self.INSTALLED = -1
+
+        self._prioritizer = GroupPrioritizer(key_func)
+        self._add_packages(self._preferred_ids.copy(), self.PREFERRED)
+        self._add_packages(self._installed_ids.copy(), self.INSTALLED)
+
+    def add_requirements(self, package_ids):
+        self._preferred_ids.difference_update(package_ids)
+        self._installed_ids.difference_update(package_ids)
+        self._add_packages(package_ids, self.REQUIRED)
+
+    def _add_packages(self, package_ids, group):
+        prioritizer = self._prioritizer
+        prioritizer.update(package_ids, group=group)
+
+        # Removing an item from an ordering always maintains the ordering,
+        # so we only need to update priorities on groups that had items added
+        for pkg_id in prioritizer.group(group):
+            if pkg_id in self._unassigned_pkg_ids:
+                self._unassigned_pkg_ids.push(pkg_id, prioritizer[pkg_id])
+
+    def get_next_package_id(self, assignments, clauses):
+        self._update_cache_from_assignments(assignments)
+
+        # Grab the most interesting looking currently unassigned id
+        return self._unassigned_pkg_ids.peek()
+
+    def _update_cache_from_assignments(self, assignments):
+        has_new_keys = assignments.has_new_keys
+        changelog = assignments.consume_changelog()
+
+        if has_new_keys:
+            unknown_ids = set(changelog).difference(self._prioritizer.known)
+            # We only want to update priorities, we'll decide who is assigned
+            # and who isn't later
+            self._prioritizer.update(unknown_ids, group=self.DEFAULT)
+
+        for key, (old, new) in six.iteritems(changelog):
+            if new is None:
+                # Newly unassigned
+                priority = self._prioritizer[key]
+                self._unassigned_pkg_ids.push(key, priority=priority)
+            elif old is None:
+                # No longer unassigned (because new is not None)
+                self._unassigned_pkg_ids.remove(key)
+
+            # The remaining case is True -> False, False -> True or
+            # MISSING -> (True|False)
+
+        # A very cheap sanity check
+        ours = len(self._unassigned_pkg_ids)
+        theirs = len(assignments) - assignments.num_assigned
+        assert ours == theirs, "We failed to track variable assignments"
+
+
+def LoggedPriorityInstalledFirstPolicty(pool, installed_repository):
+    return PolicyLogger(PriorityQueuePolicy(pool, installed_repository))
 
 
 def LoggedUndeterminedClausePolicy(pool, installed_repository):
