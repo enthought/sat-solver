fa7b0d8c5aa108e32a51f806c9e21e8e41b9db87
John Tyree <johntyree@gmail.com>
2015-12-16 17:13:18 -0600

    ENH: show detailed diff in failed test output

--- simplesat/tests/test_scenarios_policy.py
+++ simplesat/tests/test_scenarios_policy.py
@@ -9,32 +9,47 @@ from enstaller.new_solver import Pool
 from simplesat.errors import SatisfiabilityError
 from simplesat.dependency_solver import DependencySolver
 from simplesat.test_utils import Scenario
+from simplesat.transaction import (
+    InstallOperation, RemoveOperation, UpdateOperation
+)
 
 
-def _pretty_operations(ops):
-    ret = []
-    for p in ops:
-        try:
-            name = p.package.name
-            version = str(p.package.version)
-        except AttributeError:
-            name, version = p.package.split()
-        ret.append((name, version))
-    return ret
+def _pretty_operation(op):
+    if isinstance(op, InstallOperation):
+        s = "{} {}".format(op.package.name, op.package.version)
+        return "InstallOperation(package={0})".format(s)
+    if isinstance(op, RemoveOperation):
+        s = "{} {}".format(op.package.name, op.package.version)
+        return "RemoveOperation(package={0})".format(s)
+    if isinstance(op, UpdateOperation):
+        s0 = "{} {}".format(op.package.name, op.package.version)
+        s1 = "{} {}".format(op.source.name, op.source.version)
+        return "UpdateOperation(package={0}, source={1})".format(s0, s1)
+    return str(op)
 
 
 def _pkg_delta(operations, scenario_operations):
     pkg_delta = {}
     for p in operations:
-        name, version = _pretty_operations([p])[0]
-        pkg_delta.setdefault(name, [None, None])[0] = version
+        name = p.package.name
+        pkg_delta.setdefault(name, [None, None])[0] = p
     for p in scenario_operations:
-        name, version = _pretty_operations([p])[0]
-        pkg_delta.setdefault(name, [None, None])[1] = version
+        name = p.package.name
+        pkg_delta.setdefault(name, [None, None])[1] = p
     for n, v in list(six.iteritems(pkg_delta)):
-        if v[0] == v[1]:
+        if _pretty_operation(v[0]) == _pretty_operation(v[1]):
             pkg_delta.pop(n)
     return pkg_delta
+
+
+def _pretty_delta(pkg_delta):
+    lines = []
+    for k, v in sorted(pkg_delta.items()):
+        lines.append(k)
+        lines.append("  SOLVER  : {0}".format(_pretty_operation(v[0])))
+        lines.append("  SCENARIO: {0}".format(_pretty_operation(v[1])))
+        lines.append("")
+    return '\n'.join(lines)
 
 
 class ScenarioTestAssistant(object):
@@ -63,32 +78,35 @@ class ScenarioTestAssistant(object):
             if not scenario.failed:
                 msg = "Solver unexpectedly failed"
                 if failure.reason:
-                    msg += " because {}".format(failure.reason)
+                    msg += " because {0}".format(failure.reason)
                 self.fail(msg)
         else:
             if scenario.failed:
-                msg = "Solver unexpectedly succeeded, but {}."
+                msg = "Solver unexpectedly succeeded, but {0}."
                 self.fail(msg.format(scenario.failure))
             self.assertEqualOperations(transaction.operations,
                                        scenario.operations)
 
     def assertEqualOperations(self, operations, scenario_operations):
         pairs = zip(operations, scenario_operations)
+        delta = _pretty_delta(_pkg_delta(operations, scenario_operations))
         for i, (left, right) in enumerate(pairs):
             if not type(left) == type(right):
-                msg = "Item {0!r} differ in kinds: {1!r} vs {2!r}"
-                self.fail(msg.format(i, type(left), type(right)))
+                msg = "Item {0!r} differ in kinds: {1!r} vs {2!r}\n{3}"
+                self.fail(msg.format(i, type(left), type(right), delta))
 
             left_s = "{0} {1}".format(left.package.name,
                                       left.package.version)
             right_s = "{0} {1}".format(right.package.name,
                                        right.package.version)
             if left_s != right_s:
-                msg = "Item {0!r}: {1!r} vs {2!r}".format(i, left_s, right_s)
+                _pretty_delta(_pkg_delta(operations, scenario_operations))
+                msg = "Item {0!r}: {1!r} vs {2!r}\n{3}".format(
+                    i, left_s, right_s, delta)
                 self.fail(msg)
 
         if len(operations) != len(scenario_operations):
-            self.fail("Length of operations differ")
+            self.fail("Length of operations differ.\n{0}".format(delta))
 
 
 class TestNoInstallSet(ScenarioTestAssistant, TestCase):
