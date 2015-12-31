b9b82cb3d6cd5590ec23632a61446fd542bf5d44
John Tyree <johntyree@gmail.com>
2015-12-31 16:36:27 -0600

    TST: add scenario for preferring installed

--- simplesat/tests/test_scenarios_policy.py
+++ simplesat/tests/test_scenarios_policy.py
@@ -149,6 +149,9 @@ class TestInstallSet(ScenarioTestAssistant, TestCase):
     def test_update_all_conflict(self):
         self._check_solution("update_all_conflict.yaml")
 
+    def test_update_all_noop(self):
+        self._check_solution("update_all_noop.yaml")
+
     def test_ipython_upgrade(self):
         self._check_solution("ipython_upgrade.yaml")
 
