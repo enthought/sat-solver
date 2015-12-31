74990c7eb068a40558cb692c77cd5698c3f3a25f
John Tyree <johntyree@gmail.com>
2015-12-31 11:46:47 -0600

    TST: update rules prefer installed packages

---
+++ simplesat/tests/test_rules_generator.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+import unittest
+
+from enstaller.new_solver import Pool
+
+from ..rules_generator import RuleType, RulesGenerator
+from ..test_utils import Scenario
+
+
+class TestRulesGenerator(unittest.TestCase):
+
+    def test_prefer_installed(self):
+
+        self.yaml = """
+            packages:
+                - A 1.0.0-1
+            installed:
+                - A 1.0.0-1
+            marked:
+                - A
+            request:
+                - operation: "update_all"
+        """
+
+        scenario = Scenario.from_yaml(io.StringIO(self.yaml))
+        repos = list(scenario.remote_repositories)
+        repos.append(scenario.installed_repository)
+        pool = Pool(repos)
+        installed_map = {
+            pool.package_id(p): p for p in scenario.installed_repository}
+        rules_generator = RulesGenerator(pool, scenario.request, installed_map)
+        rules = list(rules_generator.iter_rules())
+
+        self.assertEqual(len(rules), 2)
+
+        (one_of, update) = rules
+        self.assertEqual(update.reason, RuleType.job_update)
+        self.assertEqual(one_of.reason, RuleType.package_same_name)
+
+        (pkg_id,) = update.literals
+        package = pool._id_to_package[pkg_id]
+
+        self.assertEqual(package.repository_info.name, "installed")
