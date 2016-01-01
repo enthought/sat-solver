1ceff9bc60496b132fc7c5ba54077a1bfcda3ea8
John Tyree <johntyree@gmail.com>
2015-12-31 20:50:39 -0600

    MAINT: remove workarounds and prep for normal PR

--- benchmarks/benchmarks/benchmarks.py
+++ benchmarks/benchmarks/benchmarks.py
@@ -4,65 +4,13 @@ import pkgutil
 
 from enstaller.new_solver import Pool
 
+from simplesat.dependency_solver import (
+    create_rules_and_initialize_policy, compute_solution_ids)
 from simplesat.errors import SatisfiabilityError
 from simplesat.sat import MiniSATSolver
 from simplesat.sat.policy import InstalledFirstPolicy
 from simplesat.test_utils import Scenario
 from simplesat.transaction import Transaction
-
-try:
-    from simplesat.dependency_solver import (
-        create_rules_and_initialize_policy, compute_solution_ids)
-except ImportError:
-    import collections
-    from operator import attrgetter
-    from egginst.errors import NoPackageFound
-    from enstaller.solver import JobType
-    from simplesat.dependency_solver import _connected_packages
-    from simplesat.rules_generator import RulesGenerator
-
-    def create_rules_and_initialize_policy(
-            pool, installed_repository, request, policy):
-
-        all_requirement_ids = []
-
-        for job in request.jobs:
-            assert job.kind in (
-                JobType.install, JobType.remove, JobType.update
-            ), 'Unknown job kind: {}'.format(job.kind)
-
-            requirement = job.requirement
-
-            providers = tuple(pool.what_provides(requirement))
-            if len(providers) == 0:
-                raise NoPackageFound(str(requirement), requirement)
-
-            if job.kind == JobType.update:
-                # An update request *must* install the latest package version
-                providers = [max(providers, key=attrgetter('version'))]
-
-            requirement_ids = [pool.package_id(p) for p in providers]
-            policy.add_requirements(requirement_ids)
-            all_requirement_ids.extend(requirement_ids)
-
-        installed_map = collections.OrderedDict()
-        for package in installed_repository:
-            installed_map[pool.package_id(package)] = package
-
-        rules_generator = RulesGenerator(pool, request, installed_map)
-        rules = list(rules_generator.iter_rules())
-
-        return (installed_map, all_requirement_ids, rules)
-
-    def compute_solution_ids(
-            pool, installed_dict, requirement_ids, solution, use_pruning=True):
-        installed_map = set(installed_dict)
-        solution_ids = sorted(solution._assigned_literals,
-                              key=lambda lit: abs(lit))
-        if use_pruning:
-            root_ids = installed_map.union(requirement_ids)
-            solution_ids = _connected_packages(solution_ids, root_ids, pool)
-        return solution_ids, installed_map
 
 
 EPD_IRIS_CONFLICT = 'epd_iris_conflict.yaml'
@@ -91,7 +39,7 @@ class STAGE(enum.Enum):
 class GraduatedSuite(object):
 
     scenario_names = (EPD_IRIS_CONFLICT, IRIS)
-    params = (scenario_names, list(STAGE)[-1:])
+    params = (scenario_names, list(STAGE))
     param_names = ("Scenario", "Stage")
 
     def _STAGES(self):
