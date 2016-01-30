f16fda716a25c34800e3739af8b09bb05d77c775
John Tyree <johntyree@gmail.com>
2016-01-29 21:09:41 -0600

    TST: explicit testing for conflict error messages and requirements

--- simplesat/tests/test_scenarios_policy.py
+++ simplesat/tests/test_scenarios_policy.py
@@ -77,20 +77,31 @@ class ScenarioTestAssistant(object):
         try:
             transaction = solver.solve(request)
         except SatisfiabilityError as failure:
-            if not scenario.failed:
-                msg = "Solver unexpectedly failed"
-                if failure.unsat:
-                    reason = failure.unsat.to_string(pool=pool)
-                    msg += ":\n{0}".format(reason)
-                    reason = failure.unsat.to_string(pool=pool, detailed=True)
-                    msg += "\n\nDetailed:\n{0}".format(reason)
-                self.fail(msg)
+            self.assertEqualFailure(pool, failure, scenario)
         else:
             if scenario.failed:
                 msg = "Solver unexpectedly succeeded, but {0}."
                 self.fail(msg.format(scenario.failure))
             self.assertEqualOperations(transaction.operations,
                                        scenario.operations)
+
+    def assertEqualFailure(self, pool, failure, scenario):
+        if not scenario.failed:
+            msg = "Solver unexpectedly failed"
+            if failure.unsat:
+                reason = failure.unsat.to_string(pool=pool)
+                msg += ":\n{0}".format(reason)
+                reason = failure.unsat.to_string(pool=pool, detailed=True)
+                msg += "\n\nDetailed:\n{0}".format(reason)
+            self.fail(msg)
+
+        req_names = [str(r) for r in failure.unsat.requirements]
+        r_names = scenario.failure['requirements']
+        self.assertEqual(req_names, r_names)
+
+        message = failure.unsat.to_string(pool=pool)
+        r_message = scenario.failure['raw']
+        self.assertEqual(message, r_message)
 
     def assertEqualOperations(self, operations, scenario_operations):
         pairs = zip(operations, scenario_operations)
