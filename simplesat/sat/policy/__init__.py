410fec9dfd909e637a7803a4b6addaf3aaf9458f
John Tyree <johntyree@gmail.com>
2016-01-29 22:11:03 -0600

    ENH: introduce the PriorityQueuePolicy again

--- simplesat/sat/policy/__init__.py
+++ simplesat/sat/policy/__init__.py
@@ -4,5 +4,8 @@ from .policy import DefaultPolicy
 from .undetermined_clause_policy import (
     LoggedUndeterminedClausePolicy, UndeterminedClausePolicy
 )
+from .priority_queue_policy import (
+    LoggedPriorityQueuePolicty, PriorityQueuePolicy
+)
 
 InstalledFirstPolicy = LoggedUndeterminedClausePolicy
