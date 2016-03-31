4b09bf6355457a536e7509deeda18e05ed3c519d
noraderam <nora.deram@gmail.com>
2016-03-31 11:14:48 -0500

    ENH: Brute force reverse dependencies

--- simplesat/compute_dependencies.py
+++ simplesat/compute_dependencies.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from simplesat.utils.graph import (package_lit_dependency_graph,
                                    transitive_neighbors)
 
@@ -24,6 +26,40 @@ def compute_dependencies(pool, requirement, transitive=False):
     for package in pool.what_provides(requirement):
         package_id = pool.package_id(package)
         dependency_ids = neighbors[package_id]
+        for d_id in dependency_ids:
+            result_dependencies.add(pool.id_to_package(d_id))
+
+    return result_dependencies
+
+
+def compute_reverse_dependencies(pool, requirement, transitive=False):
+    """ Compute packages in the given pool that depend on the given requirement
+
+    Parameters
+    ----------------
+    pool : Pool
+    requirement : Requirement
+        The package requirement for which to compute reverse dependencies.
+
+    Returns
+    -----------
+    dependencies : sequence
+         Set of packages in the pool that depend on the given requirement.
+    """
+
+    package_ids = pool._id_to_package_.keys()
+    graph = package_lit_dependency_graph(pool, package_ids, closed=False)
+    neighbors = transitive_neighbors(graph)
+
+    reversed_graph = defaultdict(set)
+    for key, vals in neighbors.iteritems():
+        for v in vals:
+            reversed_graph[v].add(key)
+
+    result_dependencies = set()
+    for package in pool.what_provides(requirement):
+        package_id = pool.package_id(package)
+        dependency_ids = reversed_graph[package_id]
         for d_id in dependency_ids:
             result_dependencies.add(pool.id_to_package(d_id))
 
--- simplesat/tests/test_compute_dependencies.py
+++ simplesat/tests/test_compute_dependencies.py
@@ -5,7 +5,8 @@ from simplesat.constraints import InstallRequirement
 from simplesat.package import PackageMetadata
 from simplesat.test_utils import pool_and_repository_from_packages
 
-from ..compute_dependencies import compute_dependencies
+from ..compute_dependencies import (compute_dependencies,
+                                    compute_reverse_dependencies)
 
 
 PACKAGE_DEF = dedent("""\
@@ -53,4 +54,34 @@ class TestComputeDependencies(unittest.TestCase):
         )
 
         deps = compute_dependencies(self.pool, requirement)
+        self.assertEqual(deps, expected_deps)
+
+
+class TestComputeReverseDependencies(unittest.TestCase):
+
+    def setUp(self):
+        self.pool, self.repo = pool_and_repository_from_packages(PACKAGE_DEF)
+
+    def _package_set_from_string(self, *package_strings):
+        packages = set()
+        for package_str in package_strings:
+            packages.add(PackageMetadata._from_pretty_string(package_str))
+
+        return packages
+
+    def test_no_dependency(self):
+        requirement = InstallRequirement._from_string('A ^= 0.0.0')
+
+        deps = compute_reverse_dependencies(self.pool, requirement)
+        self.assertEqual(deps, set())
+
+    def test_multiple_rev_dependencies(self):
+        requirement = InstallRequirement._from_string('D ^= 0.0.0')
+        expected_deps = self._package_set_from_string(
+            'A 0.0.0-1; depends (B ^= 0.0.0)',
+            'B 0.0.0-1; depends (D == 0.0.0-2)',
+            'B 0.0.0-2; depends (D ^= 0.0.0)',
+        )
+
+        deps = compute_reverse_dependencies(self.pool, requirement)
         self.assertEqual(deps, expected_deps)
