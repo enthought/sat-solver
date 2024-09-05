b2881d1e0b52ac4afe2d0bab2ad6a8af2d15aef1
Ioannis Tziakos <ioannist@enthought.com>
2024-09-05 19:51:40 +0100

    Simplify package build scripts
    - Use setup.cfg
    - Add classifiers
    - Add long description
    - Simplify setup.py and _version

--- simplesat/__init__.py
+++ simplesat/__init__.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import
-
 from .constraints import Requirement, InstallRequirement
 from .package import PackageMetadata, RepositoryPackageMetadata, RepositoryInfo
 from .pool import Pool
@@ -7,14 +5,9 @@ from .repository import Repository
 from .request import JobType, Request
 
 try:  # pragma: no cover
-    from ._version import (
-        version as __version__, version_info as __version_info__,
-        is_released as __is_released__, git_revision as __git_revision__,
-    )
+    from ._version import __version__
 except ImportError:  # pragma: no cover
-    __is_released__ = False
-    __version__ = __git_revision__ = "unknown"
-    __version_info__ = (0, 0, 0, "unknown", 0)
+    __version__ = "unknown"
 
 
 __all__ = [
@@ -27,6 +20,4 @@ __all__ = [
     'Repository',
     'JobType',
     'Request',
-    '__is_released__',
-    '__version__',
-    '__version_info__']
+    '__version__']
