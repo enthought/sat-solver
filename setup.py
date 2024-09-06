97df5e602a84f1df33609afe1b54f25e501b7155
Ioannis Tziakos <ioannist@enthought.com>
2024-09-06 13:54:41 +0100

    Fix simplesat build (#287)
    - Simplify the package build scripts (use setup.cfg)
    - Add more files and folders to sdist
    - Cleanup old dev files
    - Update workflow to fix artifact upload
    
    fixes #286

--- setup.py
+++ setup.py
@@ -1,66 +1,8 @@
-import os.path
-
+from pathlib import Path
 from setuptools import setup
 
-from setup_utils import parse_version, write_version_py
+HERE = Path(__file__).parent
+version = (HERE / 'VERSION').read_text().strip()
+filename = (HERE / 'simplesat' / '_version.py').write_text(f'__version__ = "{version}"\n')
 
-
-MAJOR = 0
-MINOR = 9
-MICRO = 0
-
-IS_RELEASED = True
-POST_RELEASE = None
-
-
-INSTALL_REQUIRES = [
-    "attrs >= 17.4.0",
-    "okonomiyaki >= 0.16.6",
-    "six >= 1.10.0"
-]
-
-EXTRAS_REQUIRE = {
-    ':python_version=="2.7"': [
-        'enum34',
-    ],
-}
-
-PACKAGES = [
-    "simplesat",
-    "simplesat.constraints",
-    "simplesat.constraints.tests",
-    "simplesat.examples",
-    "simplesat.sat",
-    "simplesat.sat.policy",
-    "simplesat.sat.tests",
-    "simplesat.tests",
-    "simplesat.test_data",
-    "simplesat.utils",
-    "simplesat.utils.tests",
-]
-
-PACKAGE_DATA = {
-    "simplesat.tests": ["*.yaml"],
-    "simplesat.test_data": ["indices/*.json"],
-}
-
-
-if __name__ == "__main__":
-    version_file = os.path.join("simplesat", "_version.py")
-    write_version_py(
-        version_file, MAJOR, MINOR, MICRO, IS_RELEASED, post_release=POST_RELEASE
-    )
-    version = parse_version(version_file)
-
-    setup(
-        name='simplesat',
-        version=version,
-        author='Enthought, Inc',
-        author_email='info@enthought.com',
-        url='https://github.com/enthought/sat-solvers',
-        description='Simple SAT solvers for use in Enstaller',
-        packages=PACKAGES,
-        package_data=PACKAGE_DATA,
-        install_requires=INSTALL_REQUIRES,
-        extras_require=EXTRAS_REQUIRE,
-    )
+setup()
